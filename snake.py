a73da096b63bb25678f805cd3f3881e5f3da5033
Harry Law <harryalaw@gmail.com>
2020-09-30 14:18:54 +0100

    Changed collision detection and added restart button

--- snake.py
+++ snake.py
@@ -9,8 +9,10 @@ GRID_HEIGHT = 500
 TOP_LEFT_X = (WIN_WIDTH-GRID_WIDTH) // 2
 TOP_LEFT_Y = (WIN_HEIGHT-GRID_HEIGHT) // 2
 CELLWIDTH = 25
-DIRECTIONS = {'Left': (-CELLWIDTH, 0), 'Right': (CELLWIDTH, 0),
-              'Up': (0, -CELLWIDTH), 'Down': (0, CELLWIDTH)}
+DIRECTIONS = {'Left': (-1, 0), 'Right': (1, 0),
+              'Up': (0, -1), 'Down': (0, 1)}
+
+DISALLOWED = {'Left': 'Right', 'Up': 'Down', 'Right': 'Left', 'Down': 'Up'}
 
 
 class Snake:
@@ -18,11 +20,11 @@ class Snake:
     def __init__(self):
         self.COLOR = (106, 238, 40)
         # @Todo assign values
-        self.length = 20
+        self.length = 3
         self.occupied_cells = deque()  # will store head at 0 and tail at -1
-        self.occupied_cells.appendleft([125, 300])
-        self.occupied_cells.appendleft([150, 300])
-        self.occupied_cells.appendleft([175, 300])
+        self.occupied_cells.appendleft([5, 12])
+        self.occupied_cells.appendleft([6, 12])
+        self.occupied_cells.appendleft([7, 12])
 
         self.dir = 'Right'
         self.xvel = DIRECTIONS[self.dir][0]
@@ -40,8 +42,8 @@ class Snake:
         # @todo implement a queue system to the inputs, so that one press corresponds to one movement
         if not self.is_alive:
             return
-        if self.frame % 2 != 0:
-            return
+        # if self.frame % 2 != 0:
+        #     return
         self.xvel = DIRECTIONS[self.dir][0]
         self.yvel = DIRECTIONS[self.dir][1]
 
@@ -52,25 +54,25 @@ class Snake:
         if len(self.occupied_cells) > self.length:
             self.occupied_cells.pop()
 
-        # collision logic:
-        self.check_collision()
-
     def check_collision(self):
         collided = False
 
-        next_head = [self.occupied_cells[0][0] +
-                     self.xvel, self.occupied_cells[0][1]+self.yvel]
-        if next_head[0] < 0 or next_head[1] < 0 or next_head[0] >= GRID_WIDTH or next_head[1] >= GRID_HEIGHT:
+        curr_head = [self.occupied_cells[0][0], self.occupied_cells[0][1]]
+        curr_head = self.occupied_cells.popleft()
+
+        if curr_head[0] < 0 or curr_head[1] < 0 or curr_head[0] >= GRID_WIDTH//CELLWIDTH or curr_head[1] >= GRID_HEIGHT//CELLWIDTH:
             collided = True
         # €todo check if this is actually how the collision logic works
-        elif next_head in self.occupied_cells and next_head != self.occupied_cells[-1]:
+
+        elif curr_head in self.occupied_cells:
             collided = True
+        self.occupied_cells.appendleft(curr_head)
         return collided
 
     def draw(self, win):
         for cell in self.occupied_cells:
             new_rect = (
-                TOP_LEFT_X+cell[0], TOP_LEFT_Y+cell[1], CELLWIDTH, CELLWIDTH)
+                TOP_LEFT_X+cell[0]*CELLWIDTH, TOP_LEFT_Y+cell[1]*CELLWIDTH, CELLWIDTH, CELLWIDTH)
             pygame.draw.rect(win, self.COLOR, new_rect)
 
     def eat(self, food):
@@ -78,25 +80,25 @@ class Snake:
             self.length += 1
             while [food.x, food.y] in self.occupied_cells:
                 food.x = random.randint(
-                    0, GRID_WIDTH//CELLWIDTH - 1) * CELLWIDTH
+                    0, GRID_WIDTH//CELLWIDTH - 1)
                 food.y = random.randint(
-                    0, GRID_HEIGHT//CELLWIDTH - 1) * CELLWIDTH
+                    0, GRID_HEIGHT//CELLWIDTH - 1)
 
 
 class Food:
     def __init__(self, snake):
-        self.x = random.randint(0, GRID_WIDTH//CELLWIDTH - 1) * CELLWIDTH
-        self.y = random.randint(0, GRID_HEIGHT//CELLWIDTH - 1) * CELLWIDTH
+        self.x = random.randint(0, GRID_WIDTH//CELLWIDTH - 1)
+        self.y = random.randint(0, GRID_HEIGHT//CELLWIDTH - 1)
 
         while [self.x, self.y] in snake.occupied_cells:
-            self.x = random.randint(0, GRID_WIDTH//CELLWIDTH - 1) * CELLWIDTH
-            self.y = random.randint(0, GRID_HEIGHT//CELLWIDTH - 1) * CELLWIDTH
+            self.x = random.randint(0, GRID_WIDTH//CELLWIDTH - 1)
+            self.y = random.randint(0, GRID_HEIGHT//CELLWIDTH - 1)
 
-        self.color = (255, 0, 0)
+        self.COLOR = (255, 0, 0)
 
     def draw(self, win):
-        pygame.draw.circle(win, self.color, (TOP_LEFT_X+self.x+CELLWIDTH //
-                                             2, TOP_LEFT_Y+self.y+CELLWIDTH//2), CELLWIDTH//2-1)
+        pygame.draw.circle(win, self.COLOR, (TOP_LEFT_X+int((self.x+0.5)*CELLWIDTH),
+                                             TOP_LEFT_Y+int((self.y+0.5)*CELLWIDTH)), CELLWIDTH//2-1)
 
 
 def draw_window(win, snake, food):
@@ -127,12 +129,20 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     snake.change_dir('Down')
+                    # snake.move()
                 elif event.key == pygame.K_LEFT:
                     snake.change_dir('Left')
+                    # snake.move()
                 elif event.key == pygame.K_UP:
                     snake.change_dir('Up')
+                    # snake.move()
                 elif event.key == pygame.K_RIGHT:
                     snake.change_dir('Right')
+                    # snake.move()
+                # reset by pressing r
+                if event.key == 114 and not snake.is_alive:
+                    snake = Snake()
+                    food = Food(snake)
         if snake.frame % 2 == 0:
             snake.move()
             snake.eat(food)
