5b021eef577fdd832258b5893eacc728f180755e
Harry Law <harryalaw@gmail.com>
2020-10-01 10:22:06 +0100

    Added score counter and changed control input to turning the snake cw/ccw

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import pygame
 import random
 from collections import deque
-
 pygame.font.init()
+
+SCORE_FONT = pygame.font.SysFont("Helvetica", 50)
+
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 600
@@ -11,24 +13,10 @@ GRID_HEIGHT = 500
 TOP_LEFT_X = (WIN_WIDTH-GRID_WIDTH) // 2
 TOP_LEFT_Y = (WIN_HEIGHT-GRID_HEIGHT) // 2
 CELLWIDTH = 25
+DIRECTIONS = {'Left': (-1, 0), 'Right': (1, 0),
+              'Up': (0, -1), 'Down': (0, 1)}
 
-# Key to remember what the
-# DIRECTIONS = {'Left': (-1, 0), 'Right': (1, 0),
-#               'Up': (0, -1), 'Down': (0, 1)}
-
-# 0 => Do nothing, 1 => Turn clockwise, 2 => turn counterclockwise, -1 => Ignore
-# VALID_MOVES = {'Up': {'Up': 0, 'Left': 1, 'Right': 2, 'Down': -1},
-#                'Left': {'Up': 2, 'Left': 0, 'Right': -1, 'Down': 1},
-#                'Down': {'Up': -1, 'Left': 2, 'Right': 1, 'Down': 0},
-#                'Right': {'Up': 1, 'Left': -1, 'Right': 0, 'Down': 2}
-#                }
-VALID_MOVES = {(0, -1): {(0, -1): 0, (-1, 0): 1, (1, 0): 2, (0, 1): -1},
-               (-1, 0): {(0, -1): 2, (-1, 0): 0, (1, 0): -1, (0, 1): 1},
-               (0, 1): {(0, -1): -1, (-1, 0): 2, (1, 0): 1, (0, 1): 0},
-               (1, 0): {(0, -1): 1, (-1, 0): -1, (1, 0): 0, (0, 1): 2}
-               }
-
-FONT = pygame.font.SysFont("comicsans", 50)
+DISALLOWED = {'Left': 'Right', 'Up': 'Down', 'Right': 'Left', 'Down': 'Up'}
 
 
 class Snake:
@@ -37,7 +25,7 @@ class Snake:
         # self.COLOR = (106, 238, 40)
         self.COLOR = (random.randint(0, 255), random.randint(
             0, 255), random.randint(0, 255))
-
+        # @Todo assign values
         self.length = 3
         self.occupied_cells = deque()  # will store head at 0 and tail at -1
 
@@ -46,37 +34,33 @@ class Snake:
         self.occupied_cells.appendleft([6, 12])
         self.occupied_cells.appendleft([7, 12])
 
-        self.xvel, self.yvel = (1, 0)
+        self.dir = 'Right'
+        self.xvel = DIRECTIONS[self.dir][0]
+        self.yvel = DIRECTIONS[self.dir][1]
+        # to peek just look at occupied_cells[0]!
 
+        self.score = 0
         self.frame = 0
         self.last_update = -1
         self.stored_change = None
         self.is_alive = True
 
-    def turn_cw(self):
-        temp = self.xvel
-        self.xvel = self.yvel
-        self.yvel = - temp
-
-    def turn_ccw(self):
-        temp = self.xvel
-        self.xvel = - self.yvel
-        self.yvel = temp
-
     def change_dir(self, dir_):
         if self.frame == self.last_update:
             self.stored_change = dir_
             return
+        if dir_ == DISALLOWED[self.dir]:
+            return
         self.last_update = self.frame
-        value = VALID_MOVES[(self.xvel, self.yvel)][dir_]
-        if value == 1:
-            self.turn_cw()
-        elif value == 2:
-            self.turn_ccw()
+        self.dir = dir_
 
     def move(self):
         if not self.is_alive:
             return
+        # if self.frame % 2 != 0:
+        #     return
+        self.xvel = DIRECTIONS[self.dir][0]
+        self.yvel = DIRECTIONS[self.dir][1]
 
         old_head = self.occupied_cells[0]
         self.occupied_cells.appendleft(
@@ -93,6 +77,7 @@ class Snake:
 
         if curr_head[0] < 0 or curr_head[1] < 0 or curr_head[0] >= GRID_WIDTH//CELLWIDTH or curr_head[1] >= GRID_HEIGHT//CELLWIDTH:
             collided = True
+        # €todo check if this is actually how the collision logic works
 
         elif curr_head in self.occupied_cells:
             collided = True
@@ -107,6 +92,7 @@ class Snake:
 
     def eat(self, food):
         if self.occupied_cells[0][0] == food.x and self.occupied_cells[0][1] == food.y:
+            self.score += 1
             self.length += 1
             while [food.x, food.y] in self.occupied_cells:
                 food.x = random.randint(
@@ -124,6 +110,7 @@ class Food:
             self.x = random.randint(0, GRID_WIDTH//CELLWIDTH - 1)
             self.y = random.randint(0, GRID_HEIGHT//CELLWIDTH - 1)
 
+        # self.COLOR = (255, 0, 0)
         self.COLOR = snake.COLOR
 
     def draw(self, win):
@@ -131,15 +118,16 @@ class Food:
                                              TOP_LEFT_Y+int((self.y+0.5)*CELLWIDTH)), CELLWIDTH//2-1)
 
 
-def draw_window(win, snake, food, score):
+def draw_window(win, snake, food):
     win.fill((0, 0, 0))
     outline = (TOP_LEFT_X, TOP_LEFT_Y, GRID_WIDTH, GRID_WIDTH)
+
     pygame.draw.rect(win, (255, 255, 255), outline, width=1)
     food.draw(win)
     snake.draw(win)
 
-    score_text = FONT.render(f"Score: {score}", 1, (255, 255, 255))
-    win.blit(score_text, (10, 10))
+    text = SCORE_FONT.render("Score: " + str(snake.score), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
 
     pygame.display.update()
 
@@ -153,19 +141,15 @@ def main():
     score = 0
     while run:
         clock.tick(15)
+        # using a frame
         # counter to be able to modulate the speed of the snake
         snake.frame += 1
-
-        start_length = snake.length
         moved = False
-        temp_dir = None
-
         if snake.stored_change:
             snake.change_dir(snake.stored_change)
             snake.move()
             snake.stored_change = None
             moved = True
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -173,35 +157,27 @@ def main():
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    temp_dir = (0, 1)
+                    snake.change_dir('Down')
+                    # snake.move()
                 elif event.key == pygame.K_LEFT:
-                    temp_dir = (-1, 0)
+                    snake.change_dir('Left')
+                    # snake.move()
                 elif event.key == pygame.K_UP:
-                    temp_dir = (0, -1)
+                    snake.change_dir('Up')
+                    # snake.move()
                 elif event.key == pygame.K_RIGHT:
-                    temp_dir = (1, 0)
-
+                    snake.change_dir('Right')
+                    # snake.move()
                 # reset by pressing r
                 if event.key == 114 and not snake.is_alive:
                     snake = Snake()
                     food = Food(snake)
-                    score = 0
-
-        if temp_dir:
-            snake.change_dir(temp_dir)
-
         if not moved:
             snake.move()
-
         snake.eat(food)
-
         if snake.check_collision():
             snake.is_alive = False
-
-        if snake.length > start_length:
-            score += 1
-
-        draw_window(win, snake, food, score)
+        draw_window(win, snake, food)
 
 
 main()
